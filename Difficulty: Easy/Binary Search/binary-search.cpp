83602ac33107c2b452e5f02355505633df07262f
karinasayta1 <karinasayta85@gmail.com>
2026-03-10 23:55:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int binarysearch(vector<int> &arr, int k) {
+        // code here
+    int left = 0;
+    int right = arr.size() - 1;
+    int ans = -1;
+
+    while (left <= right) {
+        
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == k) {
+            ans = mid;        // store index
+            right = mid - 1;  // search on left side for smaller index
+        }
+        else if (arr[mid] < k) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return ans;
+    }
+};
